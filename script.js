4016dde9a8f712e5e482d2f4963dca2d4ff9e2d5
Karin Oniesko <karinoniesko@gmail.com>
2023-06-02 16:38:24 -0300

    html pronto/atualização botão css/variáveis script

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert('Olá!')
+function gerar() {
+    var tnum = document.querySelector('input#txtnum')
+    var num = Number(tnum.value)
+    var res = document.querySelector('div#result')
+
+    res.innerHTML = `Oi ${num}`
+}
