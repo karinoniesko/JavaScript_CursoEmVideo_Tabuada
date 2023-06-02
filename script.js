d3188ff9c1b4b7aa5df55d65531e2d48f74850fe
Karin Oniesko <karinoniesko@gmail.com>
2023-06-02 17:41:27 -0300

    atualização com script funcionando

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function gerar() {
     var tnum = document.querySelector('input#txtnum')
-    var num = Number(tnum.value)
-    var res = document.querySelector('div#result')
+    var res = document.querySelector('select#result')
 
-    res.innerHTML = `Oi ${num}`
+    if (tnum.value.length == 0) {
+        window.alert('Por favor insira um número')
+    } else {
+        var num = Number(tnum.value)
+        var mult = 1
+        res.innerHTML = '' //antes de gerar a tabuada, ele limpa a área de resultado. Sem isso, cada vez que a pessoa digitar um número e gerar a tabuada, o resultado vai aparecer abaixo do que ela digitou anteriormente.
+        while (mult <= 10) {
+            var item = document.createElement('option')
+            item.text = `${num} x ${mult} = ${num*mult}`
+            res.appendChild(item)
+            mult++
+        }
+    }
 }
